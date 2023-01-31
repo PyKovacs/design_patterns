df217b9697e36bdd15935b13ea5107132d1e80f9
David Kovacs <davi.kovacs.1991@gmail.com>
2023-01-31 22:36:01 +0100

    changed to python 3.10, type fixed in select_numbers

--- strategy/main.py
+++ strategy/main.py
@@ -1,31 +1,32 @@
-from typing import Protocol, List
+from typing import Protocol
 
 class SelectionStrategy(Protocol):
 
-	def execute(self, numbers: List[int]) -> List[int]:
+	def execute(self, numbers: list[int]) -> list[int]:
 		raise NotImplementedError
 
 
 class FirstAndLastSelectionStrategy:
 
-	def execute(self, numbers: List[int]) -> List[int]:
+	def execute(self, numbers: list[int]) -> list[int]:
 		return [numbers[0], numbers[-1]]
 
 
 class EvenIndexSelectionStrategy:
 
-	def execute(self, numbers: List[int]) -> List[int]:
+	def execute(self, numbers: list[int]) -> list[int]:
 		return numbers[1::2]
 
 
-def select_numbers(numbers: List[int], selection_strategy: SelectionStrategy) -> List[int]:
+def select_numbers(numbers: list[int], selection_strategy: SelectionStrategy) -> list[int]:
 	print('Selecting the numbers...')
 	return selection_strategy.execute(numbers)
 
-def main():
-	numbers = input('Enter your numbers seperated by comma: ').split(',')
-	strategy = EvenIndexSelectionStrategy()
-	print(f'Selected numbers are: {select_numbers(numbers, strategy)}')
+def main() -> None:
+	numbers = list(map(int, input('Enter your numbers seperated by comma: ').split(',')))
+	if isinstance(numbers, list) and [item for item in numbers if isinstance(item, int)]:
+		strategy = EvenIndexSelectionStrategy()
+		print(f'Selected numbers are: {select_numbers(numbers, strategy)}')
 
 if __name__ == '__main__':
 	main()
